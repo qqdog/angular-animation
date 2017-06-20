0664bffdd5f1b0285afbab13f272a3c5c3a1baba
KevinYang <chgc.tw@gmail.com>
2017-06-20 23:59:13 +0800

    將 animation 控制的部分搬到 component.ts 內

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,15 @@
 import { Component } from '@angular/core';
-import { trigger, state, style } from '@angular/animations';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+  animation,
+  keyframes,
+  group,
+  query
+} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
@@ -7,41 +17,87 @@ import { trigger, state, style } from '@angular/animations';
   styleUrls: ['./app.component.css'],
   animations: [
     trigger('ferrisSignal', [
-      state('start', style({
-        // 'animation': 'ferris-rotate 10s infinite linear'
-      })),
-      state('stop', style({
-        'animation': 'none'
-      }))
+      transition('stop=>start', [
+        group([
+          animate(
+            '10s linear',
+            keyframes([
+              style({ transform: 'rotate(0deg)', offset: 0 }),
+              style({ transform: 'rotate(360deg)', offset: 1 })
+            ])
+          ),
+          query('.group1 .capsule', [
+            animate(
+              '10s linear',
+              keyframes([
+                style({ transform: 'rotate(360deg)', offset: 0 }),
+                style({ transform: 'rotate(0deg)', offset: 1 })
+              ])
+            )
+          ]),
+          query('.group2 .capsule', [
+            animate(
+              '10s linear',
+              keyframes([
+                style({ transform: 'rotate(300deg)', offset: 0 }),
+                style({ transform: 'rotate(-60deg)', offset: 1 })
+              ])
+            )
+          ]),
+          query('.group3 .capsule', [
+            animate(
+              '10s linear',
+              keyframes([
+                style({ transform: 'rotate(240deg)', offset: 0 }),
+                style({ transform: 'rotate(-120deg)', offset: 1 })
+              ])
+            )
+          ]),
+          query('.group4 .capsule', [
+            animate(
+              '10s linear',
+              keyframes([
+                style({ transform: 'rotate(180deg)', offset: 0 }),
+                style({ transform: 'rotate(-180deg)', offset: 1 })
+              ])
+            )
+          ]),
+          query('.group5 .capsule', [
+            animate(
+              '10s linear',
+              keyframes([
+                style({ transform: 'rotate(120deg)', offset: 0 }),
+                style({ transform: 'rotate(-240deg)', offset: 1 })
+              ])
+            )
+          ]),
+          query('.group6 .capsule', [
+            animate(
+              '10s linear',
+              keyframes([
+                style({ transform: 'rotate(60deg)', offset: 0 }),
+                style({ transform: 'rotate(-300deg)', offset: 1 })
+              ])
+            )
+          ])
+        ])
+      ]),
+      transition('start=>stop', [animate(0, style({ animate: 'none' }))])
     ]),
-    trigger('capsuleSignal', [
-      state('start', style({
-      })),
-      state('stop', style({
-        'animation': 'none'
-      }))
-    ]),
-    trigger('status', [
-      state('shaking', style({
-
-    }))
-    ])
+    trigger('status', [state('shaking', style({}))])
   ]
 })
 export class AppComponent {
   title = 'app works!';
-  ferrisSignal: string;
-  capsuleSignal: string;
+  ferrisSignal = 'stop';
   status: string;
 
   stopSignal() {
     this.ferrisSignal = 'stop';
-    this.capsuleSignal = 'stop';
   }
 
   startSignal() {
     this.ferrisSignal = 'start';
-    this.capsuleSignal = 'start';
   }
 
   shake() {
