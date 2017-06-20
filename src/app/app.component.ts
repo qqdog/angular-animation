6740344c1bf4d5a14fc5818c98195429f22ac0ae
qqdog <lianghuiru@lianghuirudeMacBook-Air.local>
2017-06-20 18:26:22 +0800

    add trigger - ferris stop and start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
 import { Component } from '@angular/core';
+import { trigger, state, style } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('ferrisSignal', [
+      state('start', style({
+        // 'animation': 'ferris-rotate 10s infinite linear'
+      })),
+      state('stop', style({
+        'animation': 'none'
+      }))
+    ]),
+    trigger('capsuleSignal', [
+      state('start', style({
+      })),
+      state('stop', style({
+        'animation': 'none'
+      }))
+    ]),
+    trigger('status', [
+      state('shaking', style({
+
+    }))
+    ])
+  ]
 })
 export class AppComponent {
   title = 'app works!';
+  ferrisSignal: string;
+  capsuleSignal: string;
+  status: string;
+
+  stopSignal() {
+    this.ferrisSignal = 'stop';
+    this.capsuleSignal = 'stop';
+  }
+
+  startSignal() {
+    this.ferrisSignal = 'start';
+    this.capsuleSignal = 'start';
+  }
+
+  shake() {
+    this.status = 'shaking';
+  }
 }
+
+// shaking not done
